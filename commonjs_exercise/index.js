7a39fc2c163352a128886aaae7c16ac77687eaa1
skplanetwalker <skplanetwalker@gmail.com>
2017-01-31 11:46:32 +0900

    commonjs 테스트(함수)

--- commonjs_exercise/index.js
+++ commonjs_exercise/index.js
@@ -3,3 +3,7 @@ console.log(myvar.a);
 
 var myliteralvar = require('./myliteralvar');
 console.log(myliteralvar.a);
+
+var myfunction = require('./myfunction');
+var myfunctiona = myfunction.a();
+console.log(myfunctiona);
