5955399b923b358d04490b2915b66f04932f0fcf
skplanetwalker <skplanetwalker@gmail.com>
2017-02-01 13:21:27 +0900

    사이트 UI 제작 - header menu 적용

--- crud_exercise/routes/auth.js
+++ crud_exercise/routes/auth.js
@@ -52,7 +52,7 @@ router.get('/facebook', passport.authenticate('facebook', { scope: 'email'}) );
 router.get('/facebook/callback',
     passport.authenticate('facebook',
         {
-            successRedirect: '/auth/facebook/success',
+            successRedirect: '/posts',
             failureRedirect: '/auth/facebook/fail'
         }
     )
