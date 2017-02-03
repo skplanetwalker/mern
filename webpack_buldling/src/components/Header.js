176bf9982f3db207de110bfe9b0daede3df6b304
skplanetwalker <skplanetwalker@gmail.com>
2017-02-03 15:22:45 +0900

    React - Event 연결

--- webpack_buldling/src/components/Header.js
+++ webpack_buldling/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 class Header extends React.Component {
+    myClick() {
+        alert('1111');
+    }
+
     render() {
         return(
-            <div>Header Component!</div>
+            <div>Header Component! <button onClick={this.myClick}>클릭</button></div>
         );
     }
 }
