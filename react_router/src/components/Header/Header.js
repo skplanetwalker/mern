2d269fe0959c1c6f9ec50feaeda8b674f9afd806
skplanetwalker <skplanetwalker@gmail.com>
2017-02-03 17:11:13 +0900

    React - Bootstrap

--- react_router/src/components/Header/Header.js
+++ react_router/src/components/Header/Header.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router';
-
+import { Navbar , Nav , NavItem , MenuItem ,NavDropdown} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class Header extends React.Component {
 
     render(){
         return(
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/posts">posts</Link></li>
-                <li><Link to="/mypage">mypage</Link></li>
-            </ul>
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">Nodejs</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to="/posts">
+                            <NavItem>Posts</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/mypage">
+                            <NavItem>Mypage</NavItem>
+                        </LinkContainer>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <MenuItem>Another action</MenuItem>
+                            <MenuItem>Something else here</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
