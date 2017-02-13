abbca943934ee38aa132c99382409fdec6d8cbae
skplanetwalker <skplanetwalker@gmail.com>
2017-02-13 17:19:21 +0900

    React - header layout 적용

--- react_express/src/components/Header.js
+++ react_express/src/components/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router';
+import { Navbar , Nav , NavItem , MenuItem ,NavDropdown} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 class Header extends React.Component {
-    render(){
 
-        return (
-            <h1>Header</h1>
+    render(){
+        return(
+            <Navbar inverse collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/">Nodejs</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to="/">
+                            <NavItem>Home</NavItem>
+                        </LinkContainer>
+                        <li><a href="/posts">Posts</a></li>
+                        <li><a href="/chat">Chat</a></li>
+                        <li><a href="/join">JOIN</a></li>
+                        <li><a href="/login">LOGIN</a></li>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
